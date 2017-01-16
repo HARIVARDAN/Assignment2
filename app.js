0244031082f404ebfde5f71ca80ce969eeaeaefc
HARIVARDAN <harivardhan.dakshana2015@gmail.com>
2017-01-16 17:31:31 +0530

    asssignment2
    Coursera

---
+++ app.js
@@ -0,0 +1,80 @@
+(function(){
+
+'use strict';
+
+angular.module('ShoppingListApp',[])
+.controller('ShoppingListBuyController',ShoppingListBuyController)
+.controller('ShoppingListBoughtController',ShoppingListBoughtController)
+.service('ShoppingListService', ShoppingListService);
+
+ShoppingListBuyController.$inject=['ShoppingListService'];
+function ShoppingListBuyController(ShoppingListService){
+
+var ShoppingList=this;
+
+   ShoppingList.items=ShoppingListService.getItems();
+
+ShoppingList.removeItem=function(itemIndex){
+    try{ ShoppingListService.removeItem(itemIndex);
+    } catch(error){
+      ShoppingList.message=error.message;
+    }
+};
+
+}
+
+
+ShoppingListBoughtController.$inject=['ShoppingListService'];
+function ShoppingListBoughtController(ShoppingListService){
+
+  var bought=this;
+  bought.items= ShoppingListService.getboughtItems();
+
+
+}
+
+function ShoppingListService(){
+  var service=this;
+  var bought_items=[];
+  var items = [
+      {
+        name: "Milk",
+        quantity: "2"
+      },
+      {
+        name: "Donuts",
+        quantity: "200"
+      },
+      {
+        name: "Cookies",
+        quantity: "300"
+      },
+      {
+        name: "Chocolate",
+        quantity: "5"
+      }
+    ];
+
+  service.getItems=function(){
+    return items;
+  };
+
+service.removeItem=function (itemIndex) {
+
+
+  bought_items.push(items[itemIndex]);
+  items.splice(itemIndex, 1);
+  if(items.length===0)
+  throw new Error("Everything is bought");
+};
+
+  service.getboughtItems = function () {
+    
+  return bought_items;
+};
+}
+
+
+
+
+})();
